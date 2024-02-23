a880e8c154f92c335b1b50adba6e0d04eeb473e8
Zachari LaCombe <zac@zachari.io>
2024-02-23 02:32:05 +0000

    Complete

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 
+
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -18,13 +20,51 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isDateValid = (dateStr) => {
+  if(dateStr)
+    return new Date(dateStr).toString() !== "Invalid Date";
+};
+
+const createDateObject = (dateStr) => {
+  if(!dateStr) {
+    return new Date();
+  } else {
+    return new Date(dateStr);
+  }
+};
+
+const createResponseObject = (date, isUnix, err) => {
+  if(err)
+    return { error: "Invalid Date" };
+  else if(isUnix)
+    return { unix: date.getTime() * 1000, utc: date.toUTCString() };
+  else
+    return { unix: date.getTime(), utc: date.toUTCString() };
+};
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', (req, res) => {
+  let dateStr = req.params.date;
+  let err = false;
+  let isUnix = false;
 
+  if(dateStr && !isNaN(dateStr)) {
+    dateStr = dateStr / 1000;
+    isUnix = true;
+  }
+
+  if(dateStr && !isDateValid(dateStr))
+    err = true;
+  
+  let date = createDateObject(dateStr);
+  let respObj = createResponseObject(date, isUnix, err);
+
+  res.json(respObj)
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
